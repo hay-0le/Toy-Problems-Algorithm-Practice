bec68b8d1b61abe1a9250eec7f847dd84287f10b
hay-0le <haley.olsen1@gmail.com>
2020-01-18 07:53:49 -0800

    Additional solution for deepEquality

--- 43-ToyProblems/W5/deepEquality.js
+++ 43-ToyProblems/W5/deepEquality.js
@@ -11,37 +11,37 @@
   * don't worry about handling cyclical object structures.
   *
   */
-//  var deepEquals = function(apple, orange) {
-//     //if strictly equal return true (when comparing values with reursion)
-//     if (apple === orange) return true;
+ var deepEquals = function(apple, orange) {
+    //if strictly equal return true (when comparing values with reursion)
+    if (apple === orange) return true;
 
-//     //if one is empty and one isn't, they wont be equal
-//     if ((!apple && orange) || (!orange & apple)) return false;
+    //if one is empty and one isn't, they wont be equal
+    if ((!apple && orange) || (!orange & apple)) return false;
     
-//     //Check to make sure both are an object
-//     if (!(apple instanceof Object) || !(orange instanceof Object)) return false;
+    //Check to make sure both are an object
+    if (!(apple instanceof Object) || !(orange instanceof Object)) return false;
 
-//     //if both objects dont have the same amount of keys they wont be equal
-//     if (Object.keys(apple).length !== Object.keys(orange).length) return false;
+    //if both objects dont have the same amount of keys they wont be equal
+    if (Object.keys(apple).length !== Object.keys(orange).length) return false;
 
-//     //If they have same about of keys and one is zero, two empty objects will be equal
-//     if (Object.keys(apple).length === 0) return true;
-
-
-//     //loop through each key in apple
-//     for (var key in apple) {
-//         // if orange does not have that key or if the keys are not the same return false
-//         if (!(orange[key] && deepEquals(apple[key], orange[key]))) {
-//             return false;;
-//         }
-//     }
-
-//     return true;
-// };
+    //If they have same about of keys and one is zero, two empty objects will be equal
+    if (Object.keys(apple).length === 0) return true;
 
 
-var deepEquals = (apple, orange) => {
-    return JSON.stringify(apple) === JSON.stringify(orange);
-}
+    //loop through each key in apple
+    for (var key in apple) {
+        // if orange does not have that key or if the keys are not the same return false
+        if (!(orange[key] && deepEquals(apple[key], orange[key]))) {
+            return false;;
+        }
+    }
+
+    return true;
+};
+
+
+// var deepEquals = (apple, orange) => {
+//     return JSON.stringify(apple) === JSON.stringify(orange);
+// }
 // console.log(deepEquals({a:1, b: {c:3}},{a:1, b: {c:3}})); // true
 console.log(deepEquals({a:1, b: {c:5}},{a:1, b: {c:6}})); // false
