7645090731f9e2a87ceee2724308cbd70102077c
hay-0le <haley.olsen1@gmail.com>
2020-02-03 15:06:20 -0800

    Added file with instructions for async map toy problem

--- 43-ToyProblems/W6/asyncMap.js
+++ 43-ToyProblems/W6/asyncMap.js
@@ -39,4 +39,31 @@ use strict';
 
 
 var asyncMap = function(tasks, callback) {
+    var results = [];
+
+    for (task of tasks) {
+        results.push(task())
+    }
+
+    callback(results);
+
 };
+
+asyncMap([
+  function(cb){
+    setTimeout(function(){
+      cb('one');
+    }, 200);
+  },
+  function(cb){
+    setTimeout(function(){
+      cb('two');
+    }, 100);
+  }
+]
+  ,
+  function(results){
+    // the results array will equal ['one','two'] even though
+    // the second function had a shorter timeout.
+    console.log(results); // ['one', 'two']
+ });
