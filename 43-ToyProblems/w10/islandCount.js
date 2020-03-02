8b7c074c1ad66b6eee42ff92ba34963a67cc59b7
hay-0le <haley.olsen1@gmail.com>
2020-03-01 21:14:42 -0800

    Prompt for islandCount (no solution)

---
+++ 43-ToyProblems/w10/islandCount.js
@@ -0,0 +1,57 @@
+/**
+ * Given a string representation of a 2d map, return the number of islands in the map. 
+ * Land spaces are denoted by a zero, while water is denoted by a dot. Two land spaces
+ * are considered connected if they are adjacent (but not diagonal).
+ *
+ */
+
+function countIslands(mapStr) {
+  let islandCount = 0;
+  let islandMap = mapStr.split('\n').map(row => row.split(''));
+
+  let deleteIsland = (r, c) => {
+    //switch coordinate to water
+    islandMap[r][c] = '.';
+
+    //check surrounding for land
+
+    //check above
+    if (islandMap[r - 1] && islandMap[r - 1] === '0') {
+      deleteIsland(r - 1, c);
+    }
+
+    //check below
+    if (islandMap[r + 1] && islandMap[r + 1][c] === '0') {
+      deleteIsland(r + 1, c);
+    }
+
+    //check left
+    if (islandMap[r][c - 1] && islandMap[r][c - 1] === '0') {
+      deleteIsland(r, c - 1);
+    }
+
+    //check right
+    if (islandMap[r][c + 1] && islandMap[r][c + 1] === '0') {
+      deleteIsland(r, c + 1);
+    }
+  }
+
+
+  for (let i = 0; i < islandMap.length; i++) {
+    for (let j = 0; j < islandMap.length; j++) {
+      if (islandMap[i][j] === '0') {
+        islandCount++;
+        deleteIsland(i, j);
+      }
+    }
+  }
+
+  return islandCount;
+    
+}
+
+console.log(countIslands(`
+..00
+...0
+..0.
+.0..`))
