623ca94aeb4dc3a42e27b986493eeb95a5de55f4
hay-0le <haley.olsen1@gmail.com>
2020-06-03 22:54:55 -0700

    Completed optimized solution for day3 of LeetCode June Challenge

--- lc-challenge-june/day3.js
+++ lc-challenge-june/day3.js
@@ -36,54 +36,30 @@
 //     let minCosts = null;
     
 //     let flyPeople = (numToCityA = 0, numToCityB = 0, currentTotal = 0, index = 0) => {
-//         if (numToCityA > costs.length / 2 || numToCityB > costs.length / 2 || (minCosts !== null && currentTotal >= minCosts)) {
-//             // console.log("NUMBER")
+//         //either city exceeds capacity
+//         if (numToCityA > costs.length / 2 || numToCityB > costs.length / 2) {
 //             return;
 //         }
 
-//         // if (minCosts !== null && currentTotal >= minCosts) {
-//         //     // console.log("TOTAL")
-//         //     return;
-//         // }
+//         //currentTotal goes over minCosts
+//         if (minCosts !== null && currentTotal >= minCosts) {
+//             return;
+//         }
 
-        
-//         // console.log(numToCityA, " : ", numToCityB)
-//         // console.log(numToCityA, numToCityB, currentTotal, index)
-//         // console.log(minCosts)
-//         //if no more people
+//         //once everyone has been sorted
 //         if (costs[index] === undefined) {
-//             //check if currentTotal is less than minCosts or first person (no minCost) and numToCityA equals numToCityB
-//             if (numToCityA === numToCityB && (currentTotal < minCosts || minCosts === null)) {
+//             //check if currentTotal is less than minCosts --> update
+//             if (currentTotal < minCosts || minCosts === null) {
 //                 minCosts = currentTotal;
-//                 return;
-//             //update minCosts to currentTotal
 //             } 
-            
 //             return;
 //         }
 
-//         // for (let i = 0; i < 2; i++) {
-//         //     return i === 0 ? flyPeople(numToCityA + 1, numToCityB, currentTotal + costs[index][0], index + 1) : 
-//         //     flyPeople(numToCityA, numToCityB + 1, currentTotal + costs[index][1], index + 1);
-
-//         // }
-
-//         return costs[index][0] < costs[index][1] ?
-//             // console.log("A")
-//             (flyPeople(numToCityA + 1, numToCityB, currentTotal + costs[index][0], index + 1),
-//             flyPeople(numToCityA, numToCityB + 1, currentTotal + costs[index][1], index + 1))
-//         :
-//             // console.log("B")
-//             (flyPeople(numToCityA, numToCityB + 1, currentTotal + costs[index][1], index + 1),
-//             flyPeople(numToCityA + 1, numToCityB, currentTotal + costs[index][0], index + 1))
-        
-//         // //for each person
-//         //     //fly cityA
-//         //     flyPeople(numToCityA + 1, numToCityB, currentTotal + costs[index][0], index + 1);
-//         //         //call flyPeople with increased numToCityA and add current persons cityA costs to currentTotal
-//         //     //flycityB
-//         //     flyPeople(numToCityA, numToCityB + 1, currentTotal + costs[index][1], index + 1);
-//         //         //call flyPeople with increased numToCityb and add current persons cityb costs to currentTotal
+//         // for each person
+//         //fly to cityA
+//         flyPeople(numToCityA + 1, numToCityB, currentTotal + costs[index][0], index + 1);
+//         //fly to cityB
+//         flyPeople(numToCityA, numToCityB + 1, currentTotal + costs[index][1], index + 1);
 //     }
 
 //     flyPeople();
@@ -112,30 +88,6 @@ var twoCitySchedCost = function(costs) {
         total:0
     }
 
-    // let i = 0;
-    
-    // while (i < costs.length) {
-    //     // console.log(i)
-    //     // console.log(costs[i][0])
-    //     if (costs[i][0] < costs[i][1]) {
-
-    //         cityA.count < costs.length/2 ? cityA = {...cityA, count: cityA.count + 1, total: cityA.total + costs[i][0]} : cityB = {...cityB, count: cityB.count + 1, total: cityB.total + costs[i][1]};
-    //     } else {
-    //         cityB.count < costs.length/2 ? cityB = {...cityB, count: cityB.count + 1, total: cityB.total + costs[i][1]} : cityA = {...cityA, count: cityA.count + 1, total: cityA.total + costs[i][0]};
-    //     }
-    //     //     cityA = {...cityA, count: cityA.count + 1, total: cityA.total + costs[i][0]};
-
-    //     // } else if (cityB.count < costs.length /2) {
-            
-    //     //     cityB = {...cityB, count: cityB.count + 1, total: cityB.total + costs[i][1]};
-    //     // } else {
-
-    //     // }
-    //     i++;
-    //     console.log("A", cityA, "....", "B", cityB)
-      
-    // }
-
     costs.forEach(x => {
         if (x[0] < x[1]) {
             cityA.count < costs.length/2 ? cityA = {...cityA, count: cityA.count + 1, total: cityA.total + x[0]} : cityB = {...cityB, count: cityB.count + 1, total: cityB.total + x[1]};
@@ -149,6 +101,20 @@ var twoCitySchedCost = function(costs) {
 
 
 
+// var twoCitySchedCost = function(costs) {
+
+//     costs.sort((a, b) => {
+//         return (a[0] - a[1]) - (b[0] - b[1])
+//     })
+
+//     total = 0;
+
+//     for (let i = 0; i < costs.length/2;i++) {
+//         total += costs[i][0] + costs[costs.length/2 + i][1];
+//     }
+
+//     return total;
+// }
 
 
 
@@ -156,7 +122,7 @@ var twoCitySchedCost = function(costs) {
 
 console.time("Total Cost")
 // console.log(twoCitySchedCost([[10,20],[30,200],[400,50],[30,20]]))
-console.log(twoCitySchedCost([[518,518],[71,971],[121,862],[967,607],[138,754],[513,337],[499,873],[337,387],[647,917],[76,417]]))
-// console.log(twoCitySchedCost([[60,749],[520,113],[951,37],[143,789],[881,936],[60,911],[531,261],[292,335],[515,462],[839,555],[268,482],[121,19],[75,942],[498,317],[499,271],[351,322],[602,169],[807,903],[154,539],[806,61],[449,889],[637,954],[505,505],[672,742],[752,782],[127,207],[457,505],[773,357],[778,890],[900,718],[692,483],[992,715],[166,694],[615,618],[426,50],[151,714],[151,434],[72,874]]))
+// console.log(twoCitySchedCost([[518,518],[71,971],[121,862],[967,607],[138,754],[513,337],[499,873],[337,387],[647,917],[76,417]]))
+console.log(twoCitySchedCost([[60,749],[520,113],[951,37],[143,789],[881,936],[60,911],[531,261],[292,335],[515,462],[839,555],[268,482],[121,19],[75,942],[498,317],[499,271],[351,322],[602,169],[807,903],[154,539],[806,61],[449,889],[637,954],[505,505],[672,742],[752,782],[127,207],[457,505],[773,357],[778,890],[900,718],[692,483],[992,715],[166,694],[615,618],[426,50],[151,714],[151,434],[72,874]]))
 // console.log(twoCitySchedCost([[60,749],[520,113],[951,37],[143,789],[881,936],[60,911],[531,261]]))
 console.timeEnd("Total Cost")
